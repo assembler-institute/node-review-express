4688ccbb17c18723a944c46deba673389edf06c5
Alejandro <alejandroaperez1994g@gmail.com>
2023-04-18 18:16:21 +0200

    feat: :sparkles: updated server config files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-const app = require("./server")
-const Config = require("./config/config")
-const connectDB = require("./db/connect")
+const app = require('./server')
+const Config = require('./config/config')
+const connectDB = require('./db/connect')
 
+connectDB().then(async function onServerInit () {
+  console.log('Database connected')
 
-connectDB().then( async function onServerInit (){
-    console.log("Database connected");
-
-    app.listen(Config.app.PORT, ()=>{
-        console.log("Server is running on port " + Config.app.PORT);
-    })
-
+  app.listen(Config.app.PORT, () => {
+    console.log('Server is running on port ' + Config.app.PORT)
+  })
 })
--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
-const express = require("express")
-const helmet = require("helmet")
-const morgan = require("morgan")
-const cors = require("cors")
-const userRoutes = require("./routes/user.routes")
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const cors = require('cors')
+const userRoutes = require('./routes/user.routes')
+const todoRoutes = require('./routes/todo.routes')
 
 const app = express()
 
 app.use(cors())
 app.use(helmet())
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 app.use(express.json())
-app.use("/user",userRoutes )
+app.use('/user', userRoutes)
+app.use('/todo', todoRoutes)
 
 module.exports = app
